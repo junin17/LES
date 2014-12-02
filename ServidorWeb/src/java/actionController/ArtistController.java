7eef8e795ce57cb785df7b7e852d693dbe70909c
junin17 <carlosarju@gmail.com>
2014-12-02 01:41:36 -0200

    Alterações

--- ServidorWeb/src/java/actionController/ArtistController.java
+++ ServidorWeb/src/java/actionController/ArtistController.java
@@ -7,11 +7,11 @@
 package actionController;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Artista;
 
 /**
  *
@@ -30,27 +30,10 @@ public class ArtistController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ArtistController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ArtistController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
+        Artista artista = new Artista();
     }
     
-    public void executarLeitura(HttpServletRequest request, HttpServletResponse response){
-        
-    }
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
