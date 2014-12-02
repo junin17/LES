b1792ebce92193eef6589b2d16378ed12778ff79
junin17 <carlosarju@gmail.com>
2014-12-02 18:10:00 -0200

    alts

--- PageController/src/java/actionController/ArtistController.java
+++ PageController/src/java/actionController/ArtistController.java
@@ -36,12 +36,6 @@ public class ArtistController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        // escreve o texto
-//        out.println("<html>");
-//        out.println("<body>");
-//        out.println("Primeira servlet");
-//        out.println("</body>");
-//        out.println("</html>");
         
         
         String nome=request.getParameter("nome");
